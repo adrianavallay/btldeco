d627b5111ec79977e564954a9c4ace428c8b9743
Adrian Avallay <adrianavallay@MacBook-Air-de-Adrian.local>
2026-04-16 12:19:55 -0300

    Bigger carousel arrows (56px)

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if (empty($galRow2)) $galRow2 = $galRow1;
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Playfair+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css?v=46">
+    <link rel="stylesheet" href="css/styles.css?v=47">
 </head>
 <body>
 
